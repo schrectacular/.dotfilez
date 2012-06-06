42dad4deb7e8847942117e5586206e7c8b44c859
Matt Schreck <mschreck@gmail.com>
2012-06-06 15:08:41 +0800

    added tmux

--- createlinks.zsh
+++ createlinks.zsh
@@ -3,3 +3,4 @@
 ln -s ~/.configs/.vimrc ~/.vimrc
 ln -s ~/.configs/.zshrc ~/.zshrc
 ln -s ~/.configs/schreck.zsh-theme ~/.oh-my-zsh/themes/schreck.zsh-theme
+ln -s ~/.configs/.tmux.conf ~/.tmux.conf
